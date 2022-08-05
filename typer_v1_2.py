f117ed2e29eefe82ed5de795aefa631916fcb199
Aaha3 <harisrinevas@gmail.com>
2022-08-05 18:45:19 +1000

    intial commit

---
+++ typer_v1_2.py
@@ -0,0 +1,34 @@
+from sqlalchemy import null
+import keyboard
+
+def hbd(text):
+    if text == "hbd":
+        print("Happy Birthday to you!")
+    else:
+        print("Have a good day")
+
+
+def print_newline(times):
+    for i in range(times):
+        print('\n')
+
+def main_block():
+    print("     typer v1.2    ")
+    server_name = input("     server name? ")
+    rangeIs = input("how long is your discussion? ")
+
+    if rangeIs.isnumeric():
+        number1 = int(rangeIs)
+        for i in range(number1):
+            print_newline(1)
+            inputZ = " [message " + "#" + server_name + "]"
+            write = input(inputZ)
+            print("you:" + write)
+            hbd(write)
+    else:
+        print('Sorry Wrong Input')
+
+if __name__ == "__main__":
+    # main_block()
+    if keyboard.is_pressed(' '):
+        print('typed space')
